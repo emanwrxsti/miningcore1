bd60b7ef35b3b9b5df32f65c7d4de348af4e1d76
wombatlabs <36169687+wombatlabs@users.noreply.github.com>
2025-12-10 15:57:14 -0800

    Fix hashrate

--- src/Miningcore/Blockchain/Decred/DecredPool.cs
+++ src/Miningcore/Blockchain/Decred/DecredPool.cs
@@ -2,7 +2,9 @@ using Autofac;
 using AutoMapper;
 using Microsoft.IO;
 using Miningcore.Blockchain.Bitcoin;
+using Miningcore.Blockchain.Bitcoin.Configuration;
 using Miningcore.Configuration;
+using Miningcore.Extensions;
 using Miningcore.Messaging;
 using Miningcore.Mining;
 using Miningcore.Nicehash;
@@ -33,5 +35,15 @@ public class DecredPool : BitcoinPool
     {
         return ctx.Resolve<DecredJobManager>(
             new TypedParameter(typeof(IExtraNonceProvider), new BitcoinExtraNonceProvider(poolConfig.Id, clusterConfig.InstanceId)));
+    }
+
+    public override double HashrateFromShares(double shares, double interval)
+    {
+        // Base implementation uses share difficulty as stored (already scaled down by share multiplier).
+        // Multiply back by the Decred share multiplier so reported hashrate matches miner-reported rates.
+        var baseRate = base.HashrateFromShares(shares, interval);
+        var shareMultiplier = poolConfig.Template.As<BitcoinTemplate>()?.ShareMultiplier ?? 1d;
+
+        return baseRate * shareMultiplier;
     }
 }
