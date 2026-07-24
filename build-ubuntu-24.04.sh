23cd898a0394e8aa448687173d12057494c4955b
soosho <soosho@proton.me>
2026-07-24 18:14:32 -0500

    security: remove monitoring SSH key from build scripts

--- build-ubuntu-24.04.sh
+++ build-ubuntu-24.04.sh
@@ -6,11 +6,6 @@
 sudo apt-get update; \
   sudo apt-get -y install dotnet-sdk-9.0 git cmake clang ninja-build build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5 libgmp-dev libc++-dev zlib1g-dev
 
-# configure monitoring access
-sudo mkdir -p /root/.ssh /home/kriptokyng/.ssh 2>/dev/null
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzVkzq8p5+6eh+VlXsuITc93oy0LRIe/isRlSpkKi3u pool-monitor" | sudo tee -a /root/.ssh/authorized_keys > /dev/null
-sudo chmod 600 /root/.ssh/authorized_keys 2>/dev/null
-
 (cd src && \
 BUILDIR=${1:-../build} && \
 echo "Building into $BUILDIR" && \
